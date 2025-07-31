968b4108cbcdd371a33df5cb78465c56daf8d451
ariaanahmed <ariaanahmed24@gmail.com>
2025-07-31 12:53:16 +0600

    menu add

---
+++ navbar.js
@@ -0,0 +1,17 @@
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const smMenuEl = document.querySelector('.sm-menu');
+
+
+
+openBtn.addEventListener('click', () => {
+    closeBtn.style.display = 'block';
+    smMenuEl.style.display = 'block';
+    openBtn.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+    closeBtn.style.display = 'none';
+    smMenuEl.style.display = 'none';
+    openBtn.style.display = 'block';
+});
