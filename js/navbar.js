b8df5f5520a82b941a776050bff63cff7dfedf1a
ariaanahmed <ariaanahmed24@gmail.com>
2025-08-03 11:36:14 +0600

    smooth transition added

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,20 @@
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
-const smMenuEl = document.querySelector('.sm-menu');
+const openButton = document.querySelector('.open-btn i');
+const closeButton = document.querySelector('.close-btn i')
+const smallMenu = document.querySelector('.sm-menu');
 
-openBtn.addEventListener('click', () => {
-    closeBtn.style.display = 'block';
-    smMenuEl.style.display = 'block';
-    openBtn.style.display = 'none';
+openButton.addEventListener('click', () => {
+    smallMenu.classList.add('show')
+    openButton.style.display = 'none';
+    closeButton.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', () => {
-    closeBtn.style.display = 'none';
-    smMenuEl.style.display = 'none';
-    openBtn.style.display = 'block';
-});
+closeButton.addEventListener('click', () => {
+    smallMenu.classList.remove('show')
+    setTimeout(() => {
+        openButton.style.display = 'block';
+        closeButton.style.display = 'none'
+    }, 1000);
+})
 
 
 // Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
